06e182098aaa873d3906318142703857295f7d02
Robert Oliveira <robertsudoliveira@gmail.com>
2021-08-25 21:28:29 -0300

    projeto finalizado com sucesso

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ var meuformulario = document.getElementById("meuformulario");
 var minhastarefas = document.getElementById("minhastarefas");
 var tarefa = document.getElementById("tarefa");
 
+var arrayDeListaDeTarefas = JSON.parse(localStorage.getItem("listadetarefas")) || [];
+
+imprimirListaHtml();
 
 meuformulario.addEventListener("submit", function (event) {
     event.preventDefault();
@@ -11,17 +14,76 @@ meuformulario.addEventListener("submit", function (event) {
         return;
     }
 
-    var novatarefa = `<li>${tarefa.value} <div> <button>tarefa concluida</button><button>deletar tarefa</button></div> </li>`;
-    
+    arrayDeListaDeTarefas.push(tarefa.value);
 
-    minhastarefas.innerHTML += novatarefa;
+    localStorage.setItem("listadetarefas", JSON.stringify(arrayDeListaDeTarefas));
+
+    imprimirListaHtml();
 
     tarefa.value = "";
 });
 
-
-function salvamento(){
-    
+function imprimirListaHtml() {
+    minhastarefas.innerHTML = "";
+    arrayDeListaDeTarefas.forEach(function(elemento, indice) {
+        minhastarefas.innerHTML += novoItemNaLista(elemento, indice);
+    });
+    adicionarEventoBotoes();
 }
 
-localStorage.setItem()
+function adicionarEventoBotoes() {
+    document.querySelectorAll(".concluido").forEach(function(botao) {
+        botao.addEventListener("click", function (event) {
+            var indicedoBotao = event.target.getAttribute("data-indice");
+            var texto = arrayDeListaDeTarefas[indicedoBotao]            
+            arrayDeListaDeTarefas[indicedoBotao] = `${texto.replaceAll("-done", "")}`;
+            localStorage.setItem("listadetarefas", JSON.stringify(arrayDeListaDeTarefas));
+            imprimirListaHtml();
+        });
+    });
+
+    document.querySelectorAll(".naoconcluido").forEach(function(botao) {
+        botao.addEventListener("click", function (event) {
+            var indicedoBotao = event.target.getAttribute("data-indice");
+            var texto = arrayDeListaDeTarefas[indicedoBotao]
+            arrayDeListaDeTarefas[indicedoBotao] = `${texto}-done`;
+            localStorage.setItem("listadetarefas", JSON.stringify(arrayDeListaDeTarefas));
+            imprimirListaHtml();
+        });
+    });
+
+    document.querySelectorAll(".deletar").forEach(function(botao) {
+        botao.addEventListener("click", function (event) {
+            var indicedoBotao = event.target.getAttribute("data-indice");
+            arrayDeListaDeTarefas.splice(indicedoBotao, 1); // alternativa
+            localStorage.setItem("listadetarefas", JSON.stringify(arrayDeListaDeTarefas));
+            imprimirListaHtml();
+        });
+    })
+}
+
+function novoItemNaLista(textoDoNovoItem, indice) {
+    var nomeDaClasse = "";
+
+    if (textoDoNovoItem.includes("-done")) {
+        nomeDaClasse = "done";
+    }
+
+    return `
+    <li class="${nomeDaClasse}">
+        ${textoDoNovoItem.replaceAll("-done", "")} 
+        <div>
+            ${nomeDaClasse == "done" ? 
+                `<button class="concluido" data-indice="${indice}">
+                    tarefa concluida
+                </button>` : 
+                `<button class="naoconcluido" data-indice="${indice}">
+                    tarefa não concluida
+                </button>`
+            }
+            <button class="deletar" data-indice="${indice}">
+                deletar tarefa
+            </button>
+        </div>
+    </li>`;
+}
